2948ab1718924e13a7b3dda01f36b87d3590e42c
Wayne C. Gramlich <Wayne@Gramlich.Net>
2016-01-19 22:33:05 -0800

    Supress filling and set stroke to .001mm

--- scad2pdf.sh
+++ scad2pdf.sh
@@ -2,7 +2,12 @@
 
 # Convert .scad file to .svg:
 openscad top_plate.scad -o top_plate.svg
-# Force the units in the .svg file to mm:
-cat top_plate.svg | sed -e 's/" height/mm" height/' -e 's/" viewBox/mm" viewBox/' > /tmp/top_plate.svg
+# Force the units in the .svg file to mm, supress filling, set stroke to .001mm:
+cat top_plate.svg | sed						\
+  -e 's/" height/mm" height/'					\
+  -e 's/" viewBox/mm" viewBox/'					\
+  -e 's/fill="lightgray"/fill="" fill-opacity="0"/'		\
+  -e 's/stroke-width="0.5"/stroke-width="0.001"/' > 		\
+  /tmp/top_plate.svg
 # Convert .svg file to .pdf:
 inkscape --file=/tmp/top_plate.svg --without-gui --export-pdf=top_plate.pdf
